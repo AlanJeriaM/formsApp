1ba19379ef7426ebd947106f32e3888f79e1b8a9
Alan Jeria Medina <alanjeria01@gmail.com>
2024-09-23 13:01:03 -0300

    Primeros pasos en formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStoage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    // Inicialización dentro del constructor
+    this.myForm = this.fb.group({
+      name: [''],
+      price: [0],
+      inStorage: [0],
+    });
+  }
+
+
+  onSave(){
+    console.log(this.myForm.value);
+
+  }
 }
