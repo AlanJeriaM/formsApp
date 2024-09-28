2e37ec189059a5556f15899edd28293a5474d52f
Alan Jeria Medina <alanjeria01@gmail.com>
2024-09-28 03:25:09 -0300

    enlazar switches en el html y ts

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
+
+public myForm: FormGroup;
+
+constructor( private fb: FormBuilder ){
+
+  this.myForm = this.fb.group({
+    gender: ['M', [Validators.required]],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+}
+
+ngOnInit(): void {
+  this.myForm.reset( this.person )
+  };
+
+
+onSave(){
+
+  if(this.myForm.invalid){
+    this.myForm.markAllAsTouched();
+    return;
+  }
+
+  const { termsAndConditions, ...newPerson } = this.myForm.value; //objeto con todas las propiedades menos el termsAndConditions
+
+  this.person = newPerson;
+  console.log(this.myForm.value);
+  console.log(this.person);
+
+
+}
+
+isValidField(field : string): boolean | null{
+  return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+}
+
+public person = {
+  gender: 'F',
+  wantNotifications: false,
+}
+
+
+
 
 }
