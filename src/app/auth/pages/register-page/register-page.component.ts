8ee1457d7539cebcab4364088dd87df667058265
Alan Jeria Medina <alanjeria01@gmail.com>
2024-10-03 18:40:59 -0300

    creando formulario de registro "myForm" en donde se creo el group con sus campos y ademas se conecto el formulario con el html.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['',[Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+
+  isValidField(field: string){
+    //TODO: obtener validaciones desde un servicio
+  }
 
 }
