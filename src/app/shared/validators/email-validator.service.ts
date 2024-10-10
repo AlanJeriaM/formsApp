5a47795d5cd730eee33a65b65f4ae0b8631ef95e
Alan Jeria Medina <alanjeria01@gmail.com>
2024-10-10 01:12:48 -0300

    se creo el email-validator que es una validacion asincrona.

---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, map, of } from 'rxjs';
+
+
+
+@Injectable({providedIn: 'root'})
+export default class EmailValidator implements AsyncValidator{
+
+   validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+    const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors | null> ( (subscriber) => {
+
+    console.log({ email });
+
+    if ( email === 'fernando@google.com' ){
+      subscriber.next({ emailTaken: true });
+      subscriber.complete();
+      // return;
+    }
+
+    subscriber.next( null );
+    subscriber.complete();
+
+
+    }).pipe(
+      delay(3000)
+    );
+
+
+  return httpCallObservable;
+
+ }
+
+
+}
+
+
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(delay(2000))
+
+  // }
+
+
+  //esto para llamarlo desde un BD
+
+  // return this.http.get<any[]>(`localhost:3000/users?q=${ email }`)
+  //           .pipe(
+  //             map(resp => {
+  //               return (resp.length === 0) ? null : {emailTaken: true}
+  //             })
+  //           );
